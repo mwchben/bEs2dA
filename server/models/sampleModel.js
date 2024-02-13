3ebbcf7bb76a8c67ac162ca9ece76da98f3843bd
Benjamin Chege <benjamwa05@gmail.com>
2024-02-13 17:36:56 +0300

    del unwanted files

--- server/models/sampleModel.js
+++
@@ -1,9 +0,0 @@
-// const mongoose = require("mongoose");
-// const { Schema } = mongoose;
-
-// const sampleSchema = new mongoose.Schema({
-//   name: String,
-//   description: String,
-// });
-
-// mongoose.model("sample", sampleSchema);
