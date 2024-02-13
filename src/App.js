e3fb5b4ab13be2d0d26ed87bb4bbc571edd53626
Benjamin Chege <benjamwa05@gmail.com>
2024-02-13 17:38:42 +0300

    removed sample

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import SampleDisplay from "./components/SampleDisplay";
 import ShowBNBS from "./components/showBNBS";
 
 function App() {
   return (
     <div className="App">
-      {/* <SampleDisplay /> */}
       <ShowBNBS />
     </div>
   );
